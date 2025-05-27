adaf028775416b5d5f0c630981f74442214c72ba
Adelphos21 <esquelat@gmail.com>
2025-05-26 19:45:13 -0500

    fix: all

--- src/main/java/com/healthcare/healthcare/usuario/repository/UserRepository.java
+++ src/main/java/com/healthcare/healthcare/usuario/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.healthcare.healthcare.usuario.repository;
 
 import com.healthcare.healthcare.usuario.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
